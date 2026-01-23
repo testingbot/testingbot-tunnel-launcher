c453487b13b509f203f53c3d40afacb66f8ba65c
Jochen Delabie <jochen@testingbot.com>
2026-01-23 17:17:05 +0100

    improve downloader for 3xx redirects just in case

--- test/downloader_test.js
+++ test/downloader_test.js
@@ -2,6 +2,7 @@ const downloader = require('./../build/downloader');
 const assert = require('assert');
 const os = require('os');
 const path = require('path');
+const fs = require('fs');
 
 describe('Downloader', function() {
 	it('should download a file correctly', function(done) {
@@ -17,6 +18,35 @@ describe('Downloader', function() {
 		const destinationPath = path.join(os.tmpdir(), "test_" + Math.round(Math.random()*10000));
 		downloader.get('https://testingbot.com/assets/this_does_not_exist.png', { destination: destinationPath }, function(err, downloadedFilePath) {
 			assert.notEqual(err, null);
+			assert.equal(downloadedFilePath, null);
+			done();
+		});
+	});
+
+	it('should follow redirects and download the file', function(done) {
+		this.timeout(10000);
+		const destinationPath = path.join(os.tmpdir(), "test_redirect_" + Math.round(Math.random()*10000));
+		const redirectUrl = 'https://httpbin.org/redirect-to?url=' + encodeURIComponent('https://testingbot.com/assets/about.png');
+		downloader.get(redirectUrl, { destination: destinationPath }, function(err, downloadedFilePath) {
+			assert.equal(err, null);
+			assert.equal(downloadedFilePath, destinationPath);
+			// Verify file was actually downloaded
+			assert.ok(fs.existsSync(destinationPath), 'Downloaded file should exist');
+			const stats = fs.statSync(destinationPath);
+			assert.ok(stats.size > 0, 'Downloaded file should not be empty');
+			// Clean up
+			fs.unlinkSync(destinationPath);
+			done();
+		});
+	});
+
+	it('should error when too many redirects occur', function(done) {
+		this.timeout(10000);
+		const destinationPath = path.join(os.tmpdir(), "test_redirect_" + Math.round(Math.random()*10000));
+		// This URL redirects 10 times, exceeding our MAX_REDIRECTS of 5
+		downloader.get('https://httpbin.org/redirect/10', { destination: destinationPath }, function(err, downloadedFilePath) {
+			assert.notEqual(err, null);
+			assert.ok(err.message.includes('Too many redirects'), 'Error should mention too many redirects');
 			assert.equal(downloadedFilePath, null);
 			done();
 		});
